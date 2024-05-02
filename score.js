458bedde8defef40ff1f8421056b23dd87e0ff75
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-05-02 16:36:07 +1200

    spare frames

--- score.js
+++ score.js
@@ -1,13 +1,21 @@
-export { scoreFrame }
+export { scoreFrame, scoreSpareFrame }
 
 // const frames = [
 //   [2, 0],
 
-function scoreFrame(frames) {
-  //let sum = 0
-  //for (let i = 0; i < frames.length; i++) {
-  const sum = frames[0] + frames[1]
-  //}
+function scoreFrame(frame) {
+  // Take sjust the one frame
+  const sum = frame[0] + frame[1]
+  // only need to call it within it's self when making tests, normally we would still need to call it.
   return sum
 }
-// scoreFrame(frames)
+
+function scoreSpareFrame(frame1, frame2) {
+  // take in one frame that will be a spare
+  // takes in the other frame that will be normal
+  // adds the first ball of the secound frame and the frist frame into "totalSpareFrame"
+  // WE DID THIS ALL BY OUR SELVES!!!
+  let totalSpareFrame = 0
+  if (scoreFrame(frame1) === 10)
+    return (totalSpareFrame = scoreFrame(frame1) + frame2[0])
+}
--- score.test.js
+++ score.test.js
@@ -13,6 +13,7 @@ test('scores a gutterball frame', () => {
   expect(actual).toBe(expected)
 })
 //originally used the var "num" but to make this work we wil use numbers as suggested in the read me
+// got help from Jarad to get the test workign again, we where trying to test a whole game not a single frame lol.
 test('scores a normal frame', () => {
   //arrange
   const frame = [3, 2]
@@ -20,5 +21,16 @@ test('scores a normal frame', () => {
   const expected = 5
   //assert
   const actual = score.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a spare frame', () => {
+  //arrange
+  const frame1 = [6, 4]
+  const frame2 = [2, 4]
+  //action
+  const expected = 12
+  //assert
+  const actual = score.scoreSpareFrame(frame1, frame2)
   expect(actual).toBe(expected)
 })
